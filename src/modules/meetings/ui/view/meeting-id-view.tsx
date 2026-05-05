a3eb0fdfd6ee317246c0dcbb809c8079829c8e08
Shubham-rawat0 <rawat07shubham@gmail.com>
2026-05-05 12:24:27 +0530

    meeting variants

--- src/modules/meetings/ui/view/meeting-id-view.tsx
+++ src/modules/meetings/ui/view/meeting-id-view.tsx
@@ -14,6 +14,11 @@ import { useConfirm } from "@/hooks/use-confirm";
 import { UpdateAgentDialog } from "@/modules/agents/ui/components/update-agent-dialog";
 import { useState } from "react";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { Divide } from "lucide-react";
+import UpcomingState from "../components/upcoming-state";
+import ActiveState from "../components/active-state";
+import CancelledState from "../components/cancelled-state";
+import ProcessingState from "../components/processing-state";
 
 interface Props {
   meetingId: string;
@@ -49,6 +54,12 @@ function MeetingIdView({ meetingId }: Props) {
     await removeMeeting.mutateAsync({ id: meetingId });
   };
 
+    const isActive = data.status === "active";
+    const isUpcoming = data.status === "upcoming";
+    const isCancelled = data.status === "cancelled";
+    const isCompleted = data.status === "completed";
+    const isProcessing = data.status === "processing";
+
   return (
     <>
       <RemoveConfirmation />
@@ -64,7 +75,15 @@ function MeetingIdView({ meetingId }: Props) {
           onEdit={() => setUpdateMeetingDialogOpen(true)}
           onRemove={handleRemoveMeeting}
         />
-        {JSON.stringify(data, null, 2)}
+        {isCancelled && <CancelledState/>}
+        {isProcessing && <ProcessingState/>}
+        {isCompleted && <div></div>}
+        {isActive && <ActiveState meetingId={meetingId}/>}
+        {isUpcoming && <UpcomingState
+            meetingId={meetingId}
+            onCancelMeeting={()=>{}}
+            isCancelling={false}
+        />}
       </div>
     </>
   );
